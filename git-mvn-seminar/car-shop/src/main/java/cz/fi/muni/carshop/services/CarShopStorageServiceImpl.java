4a7df2c574f994ba00f3ff78c4b438c9aa1c78f0
Adam Král <adam.kral@kiwi.com>
2018-09-24 16:37:18 +0200

    remove unused import

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import com.sun.javaws.exceptions.InvalidArgumentException;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
